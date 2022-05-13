b1f1e17b3a18ea56a4413dc9fc077bbc6c60af68
Champii <contact@champii.io>
2022-05-13 05:56:04 +0200

    Macro everywhere

--- src/lib.rs
+++ src/lib.rs
@@ -1,107 +1,128 @@
-use std::collections::HashMap;
+use paste::paste;
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque};
 
-trait EzIntoIterator: IntoIterator {
-    fn into_map<F, R, C>(self, f: F) -> C
-    where
-        F: FnMut(<Self as IntoIterator>::Item) -> R,
-        Self: Sized,
-        C: std::iter::FromIterator<R>,
-    {
-        Iterator::map(self.into_iter(), f).collect()
-    }
+macro_rules! eziter {
+    ($(
+        $(($($template:ident),+))? $name:ident ( $($arg:ty),* ) -> $fret:ty => $col:ty,
+    )*) => {
 
-    fn filter<F, C>(self, f: F) -> C
-    where
-        F: FnMut(&<Self as IntoIterator>::Item) -> bool,
-        Self: Sized,
-        C: std::iter::FromIterator<<Self as IntoIterator>::Item>,
-    {
-        Iterator::filter(self.into_iter(), f).collect()
-    }
+        pub trait EzRefIterator<'a>: RefIterator<'a> {
+            $(
+                fn $name<F, C $($(, $template)+)? >(&'a self, f: F) -> C
+                where
+                    Self: Sized,
+                    F: FnMut($($arg),*) -> $fret,
+                    C: std::iter::FromIterator<$col>,
+                {
+                    Iterator::$name(self.iter_ref(), f).collect()
+                }
+            )*
+        }
+
+        pub trait EzIntoIterator: IntoIterator {
+            $(
+                paste! {
+                    fn [<into_ $name>]<F, C $($(, $template)+)? >(self, f: F) -> C
+                    where
+                        Self: Sized,
+                        F: FnMut($($arg),*) -> $fret,
+                        C: std::iter::FromIterator<$col>,
+                    {
+                        Iterator::$name(self.into_iter(), f).collect()
+                    }
+                }
+            )*
+        }
+
+        pub trait EzMutIterator<'a>: MutIterator<'a> {
+            $(
+                paste! {
+                    fn [<$name _mut>]<F, C $($(, $template)+)? >(&'a mut self, f: F) -> C
+                    where
+                        Self: Sized,
+                        F: FnMut($($arg),*) -> $fret,
+                        C: std::iter::FromIterator<$col>,
+                    {
+                        Iterator::$name(self.mut_iter(), f).collect()
+                    }
+                }
+            )*
+        }
+    };
 }
 
-trait EzRefIterator<'a>: RefIterator<'a> {
-    fn map<F, R, C>(&'a self, f: F) -> C
-    where
-        F: FnMut(<Self as RefIterator>::Item) -> R,
-        Self: Sized,
-        C: std::iter::FromIterator<R>,
-    {
-        Iterator::map(self.iter_ref(), f).collect()
-    }
-
-    fn filter<F, C>(&'a self, f: F) -> C
-    where
-        F: FnMut(&<Self as RefIterator>::Item) -> bool,
-        Self: Sized,
-        C: std::iter::FromIterator<<Self as RefIterator<'a>>::Item>,
-    {
-        Iterator::filter(self.iter_ref(), f).collect()
-    }
+eziter! {
+    (R) map(Self::Item) -> R => R,
+    filter(&Self::Item) -> bool => Self::Item,
+    filter_map(Self::Item) -> Option<Self::Item> => Self::Item,
+    skip_while(&Self::Item) -> bool => Self::Item,
+    take_while(&Self::Item) -> bool => Self::Item,
+    (B) map_while(Self::Item) -> Option<B> => B,
 }
 
-trait EzMutIterator<'a>: MutIterator<'a> {
-    fn map_mut<F, R, C>(&'a mut self, f: F) -> C
-    where
-        F: FnMut(<Self as MutIterator>::Item) -> R,
-        Self: Sized,
-        C: std::iter::FromIterator<R>,
-    {
-        Iterator::map(self.mut_iter(), f).collect()
-    }
-
-    fn filter<F, C>(&'a mut self, f: F) -> C
-    where
-        F: FnMut(&<Self as MutIterator>::Item) -> bool,
-        Self: Sized,
-        C: std::iter::FromIterator<<Self as MutIterator<'a>>::Item>,
-    {
-        Iterator::filter(self.mut_iter(), f).collect()
-    }
-}
-
-trait RefIterator<'a> {
+pub trait RefIterator<'a> {
     type Item;
 
     fn iter_ref(&'a self) -> Box<dyn Iterator<Item = Self::Item> + '_>;
 }
 
-impl<'a, K: 'a, V: 'a> RefIterator<'a> for HashMap<K, V> {
-    type Item = (&'a K, &'a V);
-
-    fn iter_ref(&'a self) -> Box<dyn Iterator<Item = Self::Item> + '_> {
-        Box::new(self.iter())
-    }
-}
-
-impl<'a, T: 'a> RefIterator<'a> for Vec<T> {
-    type Item = &'a T;
-
-    fn iter_ref(&'a self) -> Box<dyn Iterator<Item = Self::Item> + '_> {
-        Box::new(self.iter())
-    }
-}
-
-trait MutIterator<'a> {
+pub trait MutIterator<'a> {
     type Item;
 
     fn mut_iter(&'a mut self) -> Box<dyn Iterator<Item = Self::Item> + '_>;
 }
 
-impl<'a, K: 'a, V: 'a> MutIterator<'a> for HashMap<K, V> {
-    type Item = (&'a K, &'a mut V);
+macro_rules! impl_eziter_ref {
+    ($(
+        $name:ty{$($param:ident),*},
+    )*) => {
+        $(
+            impl<'a, $($param: 'a),*> RefIterator<'a> for $name {
+                #[allow(unused_parens)]
+                type Item = ($(&'a $param),*);
 
-    fn mut_iter(&'a mut self) -> Box<dyn Iterator<Item = Self::Item> + '_> {
-        Box::new(self.iter_mut())
-    }
+                fn iter_ref(&'a self) -> Box<dyn Iterator<Item = Self::Item> + '_> {
+                    Box::new(self.iter())
+                }
+            }
+        )*
+    };
 }
 
-impl<'a, T: 'a> MutIterator<'a> for Vec<T> {
-    type Item = &'a mut T;
+impl_eziter_ref! {
+    HashMap<K, V>{K, V},
+    BTreeMap<K, V>{K, V},
+    HashSet<K>{K},
+    BTreeSet<K>{K},
+    BinaryHeap<T>{T},
+    LinkedList<T>{T},
+    Vec<T>{T},
+    VecDeque<T>{T},
+}
 
-    fn mut_iter(&'a mut self) -> Box<dyn Iterator<Item = Self::Item> + '_> {
-        Box::new(self.iter_mut())
-    }
+macro_rules! impl_eziter_mut {
+    ($(
+        $name:ty{$($param:ident),*}$item_mut:ty,
+    )*) => {
+        $(
+            impl<'a, $($param: 'a),*> MutIterator<'a> for $name {
+                #[allow(unused_parens)]
+                type Item = $item_mut;
+
+                fn mut_iter(&'a mut self) -> Box<dyn Iterator<Item = Self::Item> + '_> {
+                    Box::new(self.iter_mut())
+                }
+            }
+        )*
+    };
+}
+
+impl_eziter_mut! {
+    HashMap<K, V>{K, V}(&'a K, &'a mut V),
+    BTreeMap<K, V>{K, V}(&'a K, &'a mut V),
+    LinkedList<T>{T}(&'a mut T),
+    Vec<T>{T}(&'a mut T),
+    VecDeque<T>{T}(&'a mut T),
 }
 
 impl<T> EzIntoIterator for T where T: IntoIterator {}
@@ -119,7 +140,8 @@ mod tests {
         let h: HashMap<_, _> = v.into_map(|x| (x, x));
         let v2: Vec<_> = h.map(|(k, v)| k + v);
         let mut v3: Vec<_> = v2.map(|v| v + 1);
-        let _v4: Vec<_> = v3.map_mut(|v| *v + 1);
+        let v4: Vec<_> = v3.map_mut(|v| *v + 1);
+        let v5: Vec<_> = v3.filter(|v| true);
 
         assert_eq!(h.get(&1), Some(&1));
         assert_eq!(h.get(&2), Some(&2));
